f5e1e80cf91e526c47477df294a70230f8548f22
abishekvp <abishek.vp13@gmail.com>
2024-05-05 11:59:08 +0530

    tele-bot with ytvd

--- tele-bot.py
+++ tele-bot.py
@@ -1,12 +1,39 @@
-# bot.py
-import re
 import os
-import logging
-import requests
 from telegram import Update, MessageEntity
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-
+from pytube import YouTube
+from moviepy.editor import *
+import os
 TOKEN = '5148319110:AAGe4XFc8akli8Hbr2r3LF0QZPyc-WZ3Pto'
+
+AUDIO_SAVE_DIRECTORY = "E:\\Music"
+VIDEO_SAVE_DIRECTORY = "E:\\Videos"
+
+os.makedirs(os.path.dirname(VIDEO_SAVE_DIRECTORY), exist_ok=True)
+os.makedirs(os.path.dirname(AUDIO_SAVE_DIRECTORY), exist_ok=True)
+
+def download_video(video_url):
+    video = YouTube(video_url)
+    video = video.streams.get_highest_resolution()
+    for i in os.listdir(VIDEO_SAVE_DIRECTORY):
+        if str(VIDEO_SAVE_DIRECTORY+'\\'+str(video.title)+".mp4")==i:return 0
+        else:pass
+    try:video.download(VIDEO_SAVE_DIRECTORY)
+    except:return 0
+    
+def download_audio(video_url):
+    video = YouTube(video_url)
+    video = video.streams.get_highest_resolution()
+    for i in os.listdir(AUDIO_SAVE_DIRECTORY):
+        if str(AUDIO_SAVE_DIRECTORY+'\\'+str(video.title)+".mp3")==i:return 0
+        else:pass
+    try:
+        loc = video.download(AUDIO_SAVE_DIRECTORY)
+        video = VideoFileClip(loc)
+        video.audio.write_audiofile(str(loc).replace(".mp4",".mp3"))
+        video.close()
+        os.remove(str(loc).replace(".mp3",".mp4"))
+    except:return 0
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     reply_mesg = "Hi"
@@ -14,8 +41,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 async def HandleMessage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     mesg = update.message.text
-    print(mesg)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=mesg)
+    if "youtube.com" or "youtu.be" in mesg:
+        download_audio(mesg)
+        mesg = 'Audio Donwloaded'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=mesg)
 
 def main():
     application = Application.builder().token(TOKEN).build()
